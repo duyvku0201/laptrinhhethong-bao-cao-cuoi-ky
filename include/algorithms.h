1fc2a1b486e47e4583dab5388bfeeb9a762bda9a
baycao-cmd <dchnh0508@gmail.com>
2025-11-02 17:40:19 +0700

    Resolve menu.c

--- include/algorithms.h
+++ include/algorithms.h
@@ -1,1 +1,13 @@
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
 
+#include "process.h"
+
+void fcfs(Process p[], int n);
+void sjf(Process p[], int n);
+void srtf(Process p[], int n);
+void priority_non_preemptive(Process p[], int n);
+void priority_preemptive(Process p[], int n);
+void round_robin(Process p[], int n, int tq);
+
+#endif
--- include/process.h
+++ include/process.h
@@ -1,1 +1,14 @@
 
+#pragma once
+#ifndef PROCESS_H
+#define PROCESS_H
+
+// typedef struct để có thể dùng Process trực tiếp
+typedef struct Process {
+    int pid;           // Process ID
+    int arrival_time;  // Arrival Time
+    int burst_time;    // CPU Burst Time
+    int priority;      // Priority
+} Process;
+
+#endif
